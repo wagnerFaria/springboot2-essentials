744da6c9fe88d8371e6e9148b849f0a9eeb1a45d
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-02-17 14:24:13 -0400

    Spring Boot 2 Essentials 10 - Método DELETE

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -33,4 +33,11 @@ public class AnimeController {
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
+
 }
--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -36,4 +36,8 @@ public class AnimeService {
         animes.add(anime);
         return anime;
     }
+
+    public void delete(Long id) {
+        animes.remove(findByid(id));
+    }
 }
