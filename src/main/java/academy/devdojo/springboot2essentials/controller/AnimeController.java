7d72ff5b6f04a10e0f3d496c048d30faafafe92c
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-11-05 12:03:13 -0400

    Aula 08 - Método GET parte 2

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -4,7 +4,10 @@ import academy.devdojo.springboot2essentials.domain.Anime;
 import academy.devdojo.springboot2essentials.service.AnimeService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +21,12 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list() {
-        return animeService.listAll();
+    public ResponseEntity<List<Anime>> list() {
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -2,14 +2,26 @@ package academy.devdojo.springboot2essentials.service;
 
 import academy.devdojo.springboot2essentials.domain.Anime;
 import academy.devdojo.springboot2essentials.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(Anime.builder().id(1L).name("DBZ").build(), Anime.builder().id(2L).name("One Piece").build());
+
     //    private final AnimeRepository animeRepository;
     public List<Anime> listAll() {
-        return List.of(Anime.builder().id(1L).name("DBZ").build(), Anime.builder().id(2L).name("One Piece").build());
+        return animes;
+    }
+
+    public Anime findById(Long id) {
+        return animes
+                .stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
