141679480758bf0b086a271c659e0fcc0c7c6553
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-11-06 11:19:43 -0400

    Aula 09 - Método POST

--- src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,10 @@ public class AnimeController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable Long id) {
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 }
--- src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2essentials/service/AnimeService.java
@@ -6,11 +6,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(Anime.builder().id(1L).name("DBZ").build(), Anime.builder().id(2L).name("One Piece").build());
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(Anime.builder().id(1L).name("DBZ").build(), Anime.builder().id(2L).name("One Piece").build()));
+    }
 
     //    private final AnimeRepository animeRepository;
     public List<Anime> listAll() {
@@ -23,5 +29,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        AnimeService.animes.add(anime);
+        return anime;
     }
 }
