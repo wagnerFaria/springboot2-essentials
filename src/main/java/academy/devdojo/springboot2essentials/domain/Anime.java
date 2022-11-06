5d00dd16fafde7c36cdd15937aece400295c62df
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-11-06 15:50:01 -0400

    Aula 13 - Spring Data JPA pt 01

--- src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -4,13 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@Entity
+@DynamicInsert
+@DynamicUpdate
 public class Anime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
