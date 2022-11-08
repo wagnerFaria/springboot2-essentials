b2fd1641dd8899cd4e6061565810094919355424
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-11-08 10:47:50 -0400

    Aula  21 - Handler para validação de campos

--- src/main/java/academy/devdojo/springboot2essentials/handler/RestExceptionHandler.java
+++ src/main/java/academy/devdojo/springboot2essentials/handler/RestExceptionHandler.java
@@ -2,14 +2,21 @@ package academy.devdojo.springboot2essentials.handler;
 
 import academy.devdojo.springboot2essentials.exception.BadRequestException;
 import academy.devdojo.springboot2essentials.exception.BadRequestExceptionDetails;
+import academy.devdojo.springboot2essentials.exception.ValidationExceptionDetails;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Log4j2
 public class RestExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException) {
@@ -21,6 +28,26 @@ public class RestExceptionHandler {
                         .title("Bad Request Exception. Check the documentation")
                         .details(badRequestException.getMessage())
                         .developerMessage(badRequestException.getClass().getName())
+                        .build(),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
+        return new ResponseEntity<>(
+                ValidationExceptionDetails
+                        .builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, invalid fields")
+                        .details("Check the fields errors")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
                         .build(),
                 HttpStatus.BAD_REQUEST
         );
