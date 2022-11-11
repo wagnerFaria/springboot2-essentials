7da669542d0512b0407eb8a56a39fc44ac9797fc
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-11-11 14:25:31 -0400

    Aula  26 - RestTemplate getForObject e getForEntity

---
+++ src/main/java/academy/devdojo/springboot2essentials/client/SpringClient.java
@@ -0,0 +1,16 @@
+package academy.devdojo.springboot2essentials.client;
+
+import academy.devdojo.springboot2essentials.domain.Anime;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@Log4j2
+public class SpringClient {
+    public static void main(String[] args) {
+        ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);
+        log.info(entity);
+        Anime anime = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
+        log.info(anime);
+    }
+}
