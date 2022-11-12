7576d5cd5a79d5b640baa25c723a5eac62c24167
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-11-12 06:59:30 -0400

    Aula 30 - Spring Data JPA Test pt 01

---
+++ src/test/java/academy/devdojo/springboot2essentials/repository/AnimeRepositoryTest.java
@@ -0,0 +1,30 @@
+package academy.devdojo.springboot2essentials.repository;
+
+import academy.devdojo.springboot2essentials.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@DisplayName("Teste para o Anime repository")
+class AnimeRepositoryTest {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save creates anime when successful")
+    void save_PersistAnime_WhenSuccessful() {
+        Anime animeToBeSaved = createAnime();
+        Anime savedAnime = this.animeRepository.save(animeToBeSaved);
+        Assertions.assertThat(savedAnime).isNotNull();
+        Assertions.assertThat(savedAnime.getId()).isNotNull();
+        Assertions.assertThat(savedAnime.getName()).isEqualTo(animeToBeSaved.getName());
+    }
+
+    private Anime createAnime() {
+        return Anime.builder().name("Hajime no Ippo").build();
+    }
+}
