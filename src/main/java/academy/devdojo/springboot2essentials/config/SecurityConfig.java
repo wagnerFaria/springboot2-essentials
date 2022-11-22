e29ac6e66bf4925aea97f6ae3f8a83f2d0d4342e
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2022-11-22 08:38:38 -0400

    Aula  39 - Spring Security pt 01 - Autenticação em memória

---
+++ src/main/java/academy/devdojo/springboot2essentials/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package academy.devdojo.springboot2essentials.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Log4j2
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password encoder {}", passwordEncoder.encode("Test"));
+        auth
+                .inMemoryAuthentication()
+                .withUser("William")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("devdojo")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER");
+    }
+}
